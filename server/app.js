5a88068aad37ab95db56641ac5168d1b48ae0685
Shivish Makkar <makkar@wisc.edu>
2021-08-03 15:29:58 -0500

    Migrated API calls to a proxy server

--- server/app.js
+++ server/app.js
@@ -1,36 +1,31 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require('express');
+const morganLogger = require('morgan');
+const corsHeader = require('cors');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+// Initialize express App
+const app = express();
 
-var app = express();
+// Add all supported routers here
+const ticketsRouter = require('./routes/tickets');
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+app.use(corsHeader()); // Add CORS Header to API call retuned
+app.use(morganLogger('dev')); // Pass request through logger for easier debugging
+app.use(express.json()); // Recognize incoming requests as JSON objects
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+//Route used
+app.use('/tickets', ticketsRouter);
 
-// catch 404 and forward to error handler
+// Variable to store supported end points
+const supportedEndpointList = {
+  'tickets': 'GET',
+}
+
+// Catch-all error handler
 app.use(function(req, res, next) {
-  next(createError(404));
+  const error = 'Oops, you reached an unsupported endpoint. Currently supported endpoints include: \n'
+      + JSON.stringify(supportedEndpointList)
+  res.status(404).send(error);
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
 
 module.exports = app;
---
+++ server/routes/tickets.js
@@ -0,0 +1,57 @@
+const express = require('express');
+const fetch = require('node-fetch');
+const router = express.Router();
+const env = require('dotenv/config');
+
+const token = process.env.API_OAUTH_TOKEN;
+const startTime = '1625084286';
+const initialUrl = `https://zcc-shivish.zendesk.com/api/v2/incremental/tickets/cursor.json?start_time=${startTime}`;
+
+// Helper function to wait before calling an API again
+function wait(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+// Helper function to call an API
+const fetchData = async (url) => {
+    const response = await fetch(url, {
+        method: 'GET',
+        headers: {
+            'Authorization' : `Bearer ${token}`,
+            'Content-Type' : 'application/json'
+        }
+    });
+
+    // Handling API call rate limits
+    if (response.status === 429) {
+        alert('Rate limited. Waiting to retry…')
+        await wait(response.retry-after)
+        await fetchData(url)
+    }
+
+    return await response.json();
+}
+
+/* GET all current tickets from the account linked with provided token. */
+router.get('/', async function(req, res, next) {
+    // Initial API Call
+    const {tickets, end_of_stream, after_url} = await fetchData(initialUrl)
+
+    // Assigning returned values to a re-assignable value
+    let endOfStream = end_of_stream;
+    let afterUrl = after_url;
+    let totalTickets = tickets;
+
+    // Calling the API until no more data is left to be accessed
+    while (!endOfStream) {
+        const {tickets, end_of_stream, after_url} = await fetchData(afterUrl)
+
+        endOfStream = end_of_stream;
+        afterUrl = after_url;
+        totalTickets = [...totalTickets, ...tickets]
+    }
+
+    res.send(totalTickets);
+});
+
+module.exports = router;
